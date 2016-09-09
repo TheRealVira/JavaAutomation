076004e797a05e823783976cece1ebc75ce8b973
Thomas Rührig <tom.ruehrig@aon.at>
2016-09-09 13:05:39 +0200

    Added JDK installation logic

--- JavaAutomation.ps1
+++ JavaAutomation.ps1
@@ -7,18 +7,26 @@
 $currentDir = $PSScriptRoot
 $Arch = (Get-Process -Id $PID).StartInfo.EnvironmentVariables["PROCESSOR_ARCHITECTURE"];
 $global:destinationDir = ""
+$global:destinationDirJDK = ""
 $global:fileName = ""
+$global:fileNameJDK = ""
 $global:source = $null # Global, because Powershell wants it to be ¯\_(ツ)_/¯
+$global:sourceJDK = $null # Global, because Powershell wants it to be ¯\_(ツ)_/¯
 
 $source86 = "http://download.oracle.com/otn-pub/java/jdk/8u102-b14/jre-8u102-windows-i586.exe"
 $source64 = "http://download.oracle.com/otn-pub/java/jdk/8u102-b14/jre-8u102-windows-x64.exe"
+$sourceJDK86 = "http://download.oracle.com/otn-pub/java/jdk/8u102-b14/jre-8u102-windows-i586.exe"
+$sourceJDK64 = "http://download.oracle.com/otn-pub/java/jdk/8u102-b14/jdk-8u102-windows-x64.exe"
 
 # Setup variables
 Function SetupArchitectureSpecifics{
 	If($Arch -eq 'x86'){
 		Write-Host "You have an x86 system!"
 		$global:source = $source86
+		$global:sourceJDK = $sourceJDK86
 		$global:destinationDir = "C:\Program Files (x86)\Java\jre1.8.0_102\"
+		$global:destinationDirJDK = "C:\Program Files (x86)\Java\jdk1.8.0_102\"
+		$global:fileNameJDK = "jdk-8u102-windows-i586.exe"
 		$global:fileName = "jre-8u102-windows-i586.exe"
 		
 		return
@@ -26,12 +34,15 @@ Function SetupArchitectureSpecifics{
 	
 	Write-Host "You have an x64 system!"
 	$global:source = $source64
+	$global:sourceJDK = $sourceJDK64
 	$global:destinationDir = "C:\Program Files\Java\jre1.8.0_102\"
+		$global:destinationDirJDK = "C:\Program Files\Java\jdk1.8.0_102\"
+	$global:fileNameJDK = "jdk-8u102-windows-x64.exe"
 	$global:fileName = "jre-8u102-windows-x64.exe"
 }
 
-# Install Java
-Function InstallJava{
+# Install the Java_Runtime_Environment
+Function InstallJRE{
 	Write-Host source
 	Write-Host $$global:destinationDir
 	$client = New-Object System.Net.WebClient
@@ -41,9 +52,25 @@ Function InstallJava{
 	
 	& "$($currentDir)\$($global:fileName)"
 	do{
-		Read-Host 'Java isnt installed now, please come back after the installation finished...' | Out-Null
+		Read-Host 'The Java Runtime Environment isnt installed now, please come back after the installation finished...' | Out-Null
 	}
 	until(Test-Path -Path $global:destinationDir)
+}
+
+# Install the Java Development Kit
+Function InstallJDK{
+	Write-Host sourceJDK
+	Write-Host $$global:destinationDirJDK
+	$client = New-Object System.Net.WebClient
+	# Single Example
+	$client.Headers.Add([System.Net.HttpRequestHeader]::Cookie, "oraclelicense=accept-securebackup-cookie")
+	$client.DownloadFile($sourceJDK, "$($currentDir)\$($global:fileNameJDK)")
+	
+	& "$($currentDir)\$($global:fileNameJDK)"
+	do{
+		Read-Host 'The Java Development Kit isnt installed now, please come back after the installation finished...' | Out-Null
+	}
+	until(Test-Path -Path $global:destinationDirJDK)
 }
 
 # Creating Testing file (Test.java)
@@ -55,10 +82,16 @@ Function CreateFile{
 Function Main{
 	SetupArchitectureSpecifics
 	
-	# check if java is installed and if so, skip the installation:
+	# check if the jre is installed and if so, skip the installation:
 	if(!(Test-Path -Path $global:destinationDir)){
-		Write-Host "Installing Java..."
-		InstallJava
+		Write-Host "Installing the Java_Runtime_Environment..."
+		InstallJRE
+	}
+	
+	# check if the jdk is installed and if so, skip the installation:
+	if(!(Test-Path -Path $global:destinationDirJDK)){
+		Write-Host "Installing the Java Development Kit..."
+		InstallJDK
 	}
 	
 	CreateFile
