811922a9dd98d744be0e5422b29a96b4672a1191
Thomas Rührig <tom.ruehrig@aon.at>
2016-09-09 09:14:22 +0200

    Added working download logic and changed some variables to global

--- JavaAutomation.ps1
+++ JavaAutomation.ps1
@@ -6,8 +6,8 @@
 # Some need variables
 $currentDir = $PSScriptRoot
 $Arch = (Get-Process -Id $PID).StartInfo.EnvironmentVariables["PROCESSOR_ARCHITECTURE"];
-$destinationDir = ""
-$fileName = ""
+$global:destinationDir = ""
+$global:fileName = ""
 $global:source = $null # Global, because Powershell wants it to be ¯\_(ツ)_/¯
 
 $source86 = "http://download.oracle.com/otn-pub/java/jdk/8u102-b14/jre-8u102-windows-i586.exe"
@@ -18,21 +18,32 @@ Function SetupArchitectureSpecifics{
 	If($Arch -eq 'x86'){
 		Write-Host "You have an x86 system!"
 		$global:source = $source86
-		$destinationDir = "C:\Program Files (x86)\Java\"
-		$fileName = "jre-8u102-windows-i586.exe"
+		$global:destinationDir = "C:\Program Files (x86)\Java\jre1.8.0_102\"
+		$global:fileName = "jre-8u102-windows-i586.exe"
 		
 		return
 	}
 	
 	Write-Host "You have an x64 system!"
 	$global:source = $source64
-	$destinationDir = "C:\Program Files\Java\"
-	$fileName = "jre-8u102-windows-x64.exe"
+	$global:destinationDir = "C:\Program Files\Java\jre1.8.0_102\"
+	$global:fileName = "jre-8u102-windows-x64.exe"
 }
 
 # Install Java
 Function InstallJava{
-	curl -outf $currentDir $source
+	Write-Host source
+	Write-Host $$global:destinationDir
+	$client = New-Object System.Net.WebClient
+	# Single Example
+	$client.Headers.Add([System.Net.HttpRequestHeader]::Cookie, "oraclelicense=accept-securebackup-cookie")
+	$client.DownloadFile($source, "$($currentDir)\$($global:fileName)")
+	
+	& "$($currentDir)\$($global:fileName)"
+	do{
+		Read-Host 'Java isnt installed now, please come back after the installation finished...' | Out-Null
+	}
+	until(Test-Path -Path $global:destinationDir)
 }
 
 # Creating Testing file (Test.java)
@@ -45,7 +56,7 @@ Function Main{
 	SetupArchitectureSpecifics
 	
 	# check if java is installed and if so, skip the installation:
-	if(!(Test-Path -Path destinationDir)){
+	if(!(Test-Path -Path $global:destinationDir)){
 		Write-Host "Installing Java..."
 		InstallJava
 	}
