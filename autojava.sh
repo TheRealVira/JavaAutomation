ee22802d0f7a547b0e15c334ee5005ab03fc3e34
butchcom <butchcom@hotmail.de>
2016-09-15 20:56:59 +0200

    Improved Java test file generation

--- autojava.sh
+++ autojava.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo apt install -y openjdk-9-jdk
-printf 'public class Test {\npublic static void main (string[] args) {\nSystem.out.println(\"Hello world\");\n}\n}' >> Test.java
+printf "public class Test {\n\tpublic static void main (String[] args) {\n\t\tSystem.out.println(\"Hello world\");\n\t}\n}\n" >> Test.java
 echo "Java installed"
+echo "Test class in ./Test.java"
