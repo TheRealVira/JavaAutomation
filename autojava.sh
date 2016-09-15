1f6fde2d7a52dae240383f3ef0ffa23c11725dd6
butchcom <butchcom@hotmail.de>
2016-09-15 17:58:54 +0200

    Added test java file

--- autojava.sh
+++ autojava.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 sudo apt install -y openjdk-9-jdk
+printf 'public class Test {\npublic static void main (string[] args) {\nSystem.out.println(\"Hello world\");\n}\n}' >> Test.java
 echo "Java installed"
