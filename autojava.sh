f8a39b14c58b1d40a4f305c2535089cf2c510750
butchcom <butchcom@hotmail.de>
2016-09-19 10:11:53 +0200

    Added compiling feature

--- autojava.sh
+++ autojava.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 sudo apt install -y openjdk-9-jdk
 printf "public class Test {\n\tpublic static void main (String[] args) {\n\t\tSystem.out.println(\"Hello world\");\n\t}\n}\n" >> Test.java
+javac Test.java
+java Test
 echo "Java installed"
-echo "Test class in ./Test.java"
+rm Test.java Test.class
